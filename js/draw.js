42b4dcf7aa275c2476fcb81b242ac92f91162a24
mateohubert2 <mathub2003@gmail.com>
2024-05-28 13:57:57 +0200

    Partie 2 / DESSINER UN OBJET CELESTE

--- js/draw.js
+++ js/draw.js
@@ -1,11 +1,12 @@
 const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
 resize();
 
 function resize()
 {
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
-drawSquare();
+drawCelestialBody(solarSystem.sun);
 }
 window.addEventListener("resize", () => {
     resize();
@@ -13,4 +14,11 @@ window.addEventListener("resize", () => {
 
 function drawSquare(){
     console.log(solarSystem);
+}
+
+function drawCelestialBody(celestialBody){
+    context.beginPath();
+    context.arc(canvas.width / 2, canvas.height / 2, celestialBody.radius, 0, 2 * Math.PI);
+    context.fillStyle = celestialBody.color;
+    context.fill();
 }
