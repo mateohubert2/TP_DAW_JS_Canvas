ad7eff10f62cbf355ab73525578c80691afff778
mateohubert2 <mathub2003@gmail.com>
2024-05-28 13:44:10 +0200

    Partie 1 / drawSquare

--- js/draw.js
+++ js/draw.js
@@ -1,13 +1,27 @@
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
-// Starts the drawing of the shape
-context.beginPath();
-/*
-Prepares the draweing of a rectangle starting at (0 ; 0) with 100 pixels width
-and 100 pixels height
-*/
-context.rect(0, 0, 100, 100);
-// Sets the color of filling
-context.fillStyle = "#FF0000";
-// Fills the square
-context.fill();
+resize();
+
+function resize()
+{
+canvas.width = canvas.clientWidth;
+canvas.height = canvas.clientHeight;
+drawSquare();
+}
+window.addEventListener("resize", () => {
+    resize();
+});
+
+function drawSquare(){
+    const context = canvas.getContext("2d");
+    // Starts the drawing of the shape
+    context.beginPath();
+    /*
+    Prepares the draweing of a rectangle starting at (0 ; 0) with 100 pixels width
+    and 100 pixels height
+    */
+    context.rect(0, 0, 100, 100);
+    // Sets the color of filling
+    context.fillStyle = "#FF0000";
+    // Fills the square
+    context.fill();
+}
